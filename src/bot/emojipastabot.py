b15b5c9ba74958d8ed9c0d6faf7a13eb6a689a25
Kevin Galligan <galligankevinp@gmail.com>
2018-03-19 02:12:58 +0000

    User blacklist + fix for bug in infinite loop mitigation.

--- src/bot/emojipastabot.py
+++ src/bot/emojipastabot.py
@@ -15,9 +15,20 @@ from emojipasta.emojipastagenerator import EmojipastaGenerator
 SECONDS_BETWEEN_RUNS = 60
 SECONDS_TO_WAIT_AFTER_RATE_LIMITING = 600
 
+def normalize_name(name):
+    return name.lower()
+
+RAW_USER_BLACKLIST = [
+    # A bot that keeps turning up whenever someone messes
+    # up the tag, causes feedback loop with emojipasta bot.
+    "Sub_Corrector_Bot"
+]
+USER_BLACKLIST = [normalize_name(name) for name in RAW_USER_BLACKLIST]
+
 class EmojipastaBot:
 
     MAX_NUM_TAGS_PER_USER_IN_CHAIN = 3
+    COMMENT_LOG_INDENT = 4
 
     """Basic constructor.
 
@@ -41,21 +52,23 @@ class EmojipastaBot:
         log("Reading inbox...")
         for mention in self._inbox.unread(limit=None):
             if isinstance(mention, Comment) and self._was_tagged_in(mention):
-                log("====== Comment")
-                log("Received comment from u/" + author_name(mention) + ".")
+                log("Received comment:")
+                log("Author is u/" + author_name(mention) + ".", EmojipastaBot.COMMENT_LOG_INDENT)
                 should_reply = True
                 if self._user_has_tagged_too_many_times_in_thread(mention):
                     should_reply = False
-                    log("User has tagged too many times in this comment chain, ignoring.")
+                    log("Author has tagged too many times in this comment chain, ignoring.", EmojipastaBot.COMMENT_LOG_INDENT)
+                if self._author_is_in_blacklist(mention):
+                    should_reply = False
+                    log("Author is blacklisted, ignoring.", EmojipastaBot.COMMENT_LOG_INDENT)
                 got_rate_limited = False
                 if should_reply:
                     got_rate_limited = self._attempt_reply(mention)
                 if got_rate_limited:
                     self._wait_for_rate_limiting_to_pass()
                 else:
-                    log("Comment has been processed!!")
+                    log("Comment processed, marking as read.", EmojipastaBot.COMMENT_LOG_INDENT)
                     self._inbox.mark_read([mention])
-                log("========")
         log("Finished reading inbox.")
         log("========")
 
@@ -68,10 +81,13 @@ class EmojipastaBot:
         tags = 0
         comment = mention
         while not comment.is_root:
-            if author_name(mention) == author_name(mention) and self._was_tagged_in(comment):
+            if author_name(comment) == author_name(mention) and self._was_tagged_in(comment):
                 tags += 1
             comment = comment.parent()
         return tags > EmojipastaBot.MAX_NUM_TAGS_PER_USER_IN_CHAIN
+
+    def _author_is_in_blacklist(self, comment):
+        return author_name(comment) in USER_BLACKLIST
 
     """Attempts reply, failures due to API exceptions are caught.
 
@@ -80,22 +96,23 @@ class EmojipastaBot:
     """
     def _attempt_reply(self, mention):
         text_of_parent = get_text_of_parent(mention)
-        log("Text of parent is: " + text_of_parent)
+        log("Text of parent is: " + text_of_parent, EmojipastaBot.COMMENT_LOG_INDENT)
         emojipasta = self._emojipasta_generator.generate_emojipasta(text_of_parent)
-        log("Generated emojipasta: " + emojipasta)
+        log("Generated emojipasta: " + emojipasta, EmojipastaBot.COMMENT_LOG_INDENT)
         try:
             mention.reply(emojipasta)
         except praw.exceptions.APIException as e:
-            log("API exception: " + e.message)
+            log("API exception: " + e.message, EmojipastaBot.COMMENT_LOG_INDENT)
             if e.error_type == "RATELIMIT":
                 return True
         return False
 
     def _wait_for_rate_limiting_to_pass(self):
-        log("Got rate-limited, can't reply yet.")
+        log("Got rate-limited, can't reply yet.", EmojipastaBot.COMMENT_LOG_INDENT)
         log(
             "Waiting %d seconds for rate-limiting to wear off, then can try again"
-                .format(SECONDS_TO_WAIT_AFTER_RATE_LIMITING))
+                .format(SECONDS_TO_WAIT_AFTER_RATE_LIMITING),
+            EmojipastaBot.COMMENT_LOG_INDENT)
         time.sleep(SECONDS_TO_WAIT_AFTER_RATE_LIMITING)
 
 def get_text_of_parent(comment):
@@ -108,10 +125,10 @@ def get_text_of_parent(comment):
 
 """Returns normalized name of the comment's author."""
 def author_name(comment):
-    return comment.author.name.lower()
+    return normalize_name(comment.author.name)
 
-def log(message):
-    print(message)
+def log(message, indent=0):
+    print((indent * " ") + message)
 
 def main():
     bot = EmojipastaBot(get_reddit(sys.argv), EmojipastaGenerator.of_default_mappings())
