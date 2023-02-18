88f24d9e67fb2379b5c03d4bb3ab67134588bea9
Silas <63477128+SilasStokes@users.noreply.github.com>
2023-02-18 14:44:08 -0800

    fixed interface with emoji module

--- src/emojipasta/util/emoji.py
+++ src/emojipasta/util/emoji.py
@@ -4,7 +4,7 @@ Common emoji code.
 
 import emoji
 
-EMOJIS = set(emoji.emojize(emoji_code) for emoji_code in emoji.UNICODE_EMOJI.values())
+EMOJIS = set(emoji.emojize(emoji_code['en']) for emoji_code in emoji.EMOJI_DATA.values())
 
 def is_emoji(c):
     return c in EMOJIS
